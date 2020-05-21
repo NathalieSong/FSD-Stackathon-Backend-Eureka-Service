3809ef1046dfed219f230432472e58fafe97d125
Yue Juan Song <syjsong@cn.ibm.com>
2020-05-21 14:43:08 +0800

    Init

---
+++ src/main/java/com/emart/eureka/service/eurekaservice/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.emart.eureka.service.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emart/eureka/service/eurekaservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emart.eureka.service.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
